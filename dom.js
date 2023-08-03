5ea4531b64063f9941d88f21c543917f49e65ae8
shubham8603 <shubham8603@gmail.com>
2023-08-04 00:16:05 +0530

    querySelectorAll

--- dom.js
+++ dom.js
@@ -110,18 +110,21 @@ for(var i=0;i<Items2.length;i++)
 
 
 
-var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+/*var secondItem=document.querySelector('.list-group-item:nth-child(2)');
 secondItem.style.backgroundColor='green';
 var thirdItem=document.querySelector('.list-group-item:nth-child(3)');
 thirdItem.remove(thirdItem);
-
+*/
 
 //trying to access by querySelectorAll
 
 
-/*var odd=document.querySelectorAll('li:nth-child(odd)');
+var odd=document.querySelectorAll('li:nth-child(odd)');
+var secondItem=document.querySelectorAll('li:nth-child(even)');
+
+secondItem[0].style.color='green';
 for(var i=0;i<odd.length;i++)
 {
-    odd[i].style.color ='green';
+    odd[i].style.backgroundColor ='green';
       
-}*/
+}
